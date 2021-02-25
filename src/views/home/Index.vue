e2f51c82d745fd230de175713d484c6238f4e6ea
jbsethi <jahanzebsethi@gmail.com>
2021-02-25 23:06:26 +0500

    Remove repeat experiances

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -26,9 +26,6 @@
     <section class="testimonials-ui">
       <testimonial />
     </section>
-    <section class="previous-experience py-5">
-      <previous-experience />
-    </section>
     <section class="newsletter-ui py-5">
       <newsletter />
     </section>
@@ -45,7 +42,7 @@ import PreviousExperience from "../../components/home/PreviousExperience.vue";
 import Service from "../../components/home/Service.vue";
 import Testimonial from "../../components/home/Testimonial.vue";
 import Newsletter from "../../components/home/Newsletter.vue";
-import HomeFooter from '../../components/home/HomeFooter.vue';
+import HomeFooter from "../../components/home/HomeFooter.vue";
 export default {
   components: {
     CustomButton,
