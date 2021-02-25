79eb0d3af2117d71979a72d57136bf2dd18d4297
jonacruz948 <58448072+jonacruz948@users.noreply.github.com>
2021-02-25 10:23:48 -0800

    Development (#14)
    * navbar design done
    
    * typewriter effect added.
    
    * typewriter uninstalled
    
    * typewriter font changed
    
    * format of links changed
    
    * image added dynamically
    
    * change nabvar content
    
    * portfolio UI done
    
    * lint fixes
    
    * change header title
    
    * change header
    
    * change header
    
    * Fix top nav bar UI issue on mobile
    Make hero image responsive on homepage
    
    * Update links and button
    
    * Fix text wrapper for tablet screens
    
    * UI fix for initial screen
    
    * portfolio adjusted
    
    * Complete experiance section
    Add in testimonials section
    
    * Remove decorator for link
    
    * Add cards for testimonials
    
    * Breakdown into components for resuable
    
    * change lock.json
    
    * change section ui
    
    * change section ui
    
    * Add Testimonials
    Add Services
    Add Footer
    Add Ask a question
    
    * Remove repeat experiances
    
    * Remove Action from services
    
    Co-authored-by: godofjs <henryeze019@gmail.com>
    Co-authored-by: comet-hale772 <comethale772@gmail.com>
    Co-authored-by: jbsethi <jahanzebsethi@gmail.com>

---
+++ src/components/home/Service.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="py-lg-5 pb-5 row">
+    <div class="col-sm-12 col-md-6">
+      <h3 class="service-heading py-md-4 text-left">
+        Parachains and Applications
+      </h3>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <ul class="service-list d-flex flex-column pl-lg-0 pl-5 py-4 text-left">
+        <li
+          v-for="(service, index) in services"
+          :key="index"
+          class="service-list__item px-4 mb-3"
+        >
+          {{ service }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const services = ref([
+      "Gaming and eSports",
+      "Messaging",
+      "DAO",
+      "Marketplaces",
+      "Oracles",
+      "Music",
+      "Social Networking"
+    ]);
+    return {
+      services
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/shared/mixins.scss";
+@import "@/scss/colors.scss";
+
+.service-heading {
+  color: $white;
+  font-size: 1.75em;
+  font-weight: 600;
+  line-height: 2;
+  @include phone-screen() {
+    font-size: 1.5em;
+  }
+  @include tablet-screen() {
+    font-size: 1.5em;
+  }
+}
+
+.service-list {
+  list-style: none;
+  &__item {
+    @include space-mono-font();
+    font-size: 1.4em;
+    line-height: 2;
+    color: $white;
+    @include phone-screen() {
+      font-size: 1.25em;
+    }
+    @include tablet-screen() {
+      font-size: 1.25em;
+    }
+  }
+}
+</style>
